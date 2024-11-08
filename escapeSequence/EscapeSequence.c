babe2167f25d0b6dd207b76b64a2c71dea7094e1
Muhammad Ali <proqualitythings100@gmail.com>
2024-11-08 22:03:33 +0500

    escape sequences

---
+++ escapeSequence/EscapeSequence.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    printf("I\nam\nAli\n"); // \n for new line
+
+    printf("1\t2\t3\t4 \n"); // \t for tab space
+
+    printf("\"Feel the fear and do it anyway\" - Muhammad Ali");
+
+    return 0;
+}
