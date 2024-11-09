4e967f13b9006f5033d126906a8e39a7e438bc90
Muhammad Ali <proqualitythings100@gmail.com>
2024-11-09 11:24:13 +0500

    variables

---
+++ variables/variables.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    
+    int age; // declaration
+    age = 20; // initialization Integer
+
+    // declaration + initialization
+
+    float gpa = 3.8; // floating point number
+
+    char grade = 'C'; // single character
+
+    char name[] = "Muhammad Ali"; // array of characters
+
+    printf("My name is %s\n", name);
+    printf("I'm %d years old.\n", age);
+    printf("Your average grade is %c\n", grade);
+    printf("Your GPA is %f\n", gpa);
+
+    return 0;
+}
