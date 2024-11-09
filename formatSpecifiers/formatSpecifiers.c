9cfd989c5cd4afb6e3e360abce45a8166eb399a1
Muhammad Ali <proqualitythings100@gmail.com>
2024-11-09 11:48:27 +0500

    format specifiers

---
+++ formatSpecifiers/formatSpecifiers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    float item1 = 5.75;
+    float item2 = 10.99;
+    float item3 = 53.99;
+
+    printf("Item 1: $%.2f\n", item1);
+    printf("Item 2: $%.2f\n", item2);
+    printf("Item 3: $%.2f", item3);
+
+    return 0;
+}
