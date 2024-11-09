eb359cdbae12c1a6e1d985e6238872baccea7c32
Muhammad Ali <proqualitythings100@gmail.com>
2024-11-09 11:40:51 +0500

    data types

---
+++ dataTypes/dataTypes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
+
+    char a = "A";  // single character %c
+    char name[] = "Muhammad Ali"; // string %s
+
+    float num = 3.234523; // %f
+    double num2 = 3.25012523105; // %lf
+
+    bool isAlive = true; // boolean true or false %d
+
+    return 0;
+}
