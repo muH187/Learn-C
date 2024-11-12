5e152d4dbd012cc6f041dadeb29d98ec003016d0
Muhammad Ali <proqualitythings100@gmail.com>
2024-11-12 11:02:36 +0500

    string method

---
+++ string/string.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+
+    char string1[] = "Muhammad";
+    char string2[] = "Ali";
+
+    // strlwr(string1);
+    // strupr(string1);
+    // strcat(string1, string2);
+    // strcpy(string1, string2);
+    // strset(string1, 'y');
+    // strnset(string1, 'o', 1);
+    // strrev(string1);
+
+    // printf("%s", string1);
+    
+
+    int result = strlen(string1);
+
+    printf("%d", result);
+
+    return 0;
+}
